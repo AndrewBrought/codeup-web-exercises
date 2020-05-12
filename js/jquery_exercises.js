ad5d0fd8555e719597ecfaec13f50eac7337f16d
Andrew Brought <andrewbrought@gmail.com>
2020-05-12 11:02:44 -0500

    walkthrough of using IIFE with document ready

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,8 +1,15 @@
-"use strict"
+"use strict";
 
-// $(document).ready(function() {
-//     alert( 'The DOM has finished loading!' );
-// });
-    $(document).ready(function() {
-       alert('The DOM has finished loading!');
-    });
+$(document).ready(function() {
+    alert('The DOM has finished loading!');
+});
+
+    //Using an IIFE in combination with jQuery DOM ready
+// (function($){
+//     $(document).ready(function() {
+//        alert('The DOM has finished loading!');
+        // JAVASCRIPT GOES HERE
+//     });
+//
+// })(jQuery); // how to use IIFE with jQuery
+
