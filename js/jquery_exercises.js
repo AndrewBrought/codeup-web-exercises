c6220ab863ff2e4c4b78499b58408d47ae011967
Andrew Brought <andrewbrought@gmail.com>
2020-05-12 13:37:31 -0500

    completed ID selectors for jquery_exercises

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,7 +1,17 @@
 "use strict";
 
 $(document).ready(function() {
-    alert('The DOM has finished loading!');
+
+    // var title = $('#welcome-title').html();
+    //     alert(title);
+
+    // var paragraphOne = $('#first-paragraph').html();
+    //     alert(paragraphOne);
+
+    var games = $('#game').html();
+        alert(games);
+
+
 });
 
     //Using an IIFE in combination with jQuery DOM ready
@@ -12,4 +22,5 @@ $(document).ready(function() {
 //     });
 //
 // })(jQuery); // how to use IIFE with jQuery
+
 
